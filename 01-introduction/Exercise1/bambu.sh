e8cfd0a65d5fb2e664299c36e0834cdd687b1a6b
curzel <serena.curzel@mail.polimi.it>
2021-06-11 18:40:34 +0200

    fixed file names

--- 01-introduction/Exercise1/bambu.sh
+++ 01-introduction/Exercise1/bambu.sh
@@ -4,7 +4,7 @@ export PATH=/opt/panda/bin:$PATH
 mkdir -p icrc1
 cd icrc1
 echo "#synthesis of icrc1"
-bambu ../module.c --top-fname=icrc1 --simulator=VERILATOR --simulate --generate-tb=../test_icrc1.xml -v2 --print-dot --pretty-print=a.c 2>&1 | tee icrc1.log
+bambu ../icrc.c --top-fname=icrc1 --simulator=VERILATOR --simulate --generate-tb=../test_icrc1.xml -v2 --print-dot --pretty-print=a.c 2>&1 | tee icrc1.log
 cd ..
 
 
