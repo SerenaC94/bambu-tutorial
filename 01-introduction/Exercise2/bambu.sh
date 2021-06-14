ee46f19ad9a4a62e8f86a0d1f6356c9b27566f52
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2021-06-14 12:49:06 +0200

    Changed simulator to second exercise of the intro.

--- 01-introduction/Exercise2/bambu.sh
+++ 01-introduction/Exercise2/bambu.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 export PATH=/opt/panda/bin:$PATH
 
-mkdir -p pop
-cd pop
-echo "#synthesis of search function"
-bambu ../tree.c --top-fname=main --top-rtldesign-name=search --simulate -v2 -DNDEBUG -DBAMBU_PROFILING --generate-tb=../test_pop.xml -v2 --print-dot --pretty-print=a.c 2>&1 | tee pop.log
+mkdir -p search
+cd search
+echo "#simulation of search function"
+bambu ../tree.c --top-fname=main --top-rtldesign-name=search --simulate -v2 -DNDEBUG -DBAMBU_PROFILING --simulator=ICARUS --generate-tb=../test_search.xml -v2 --print-dot 2>&1 | tee pop.log
 cd ..
 
 
