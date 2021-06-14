66f4e09ebbbc412b156fe4ac892b4597369766dc
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2021-06-14 13:21:05 +0200

    Fixed second example of the intro.

--- 01-introduction/Exercise2/bambu.sh
+++ 01-introduction/Exercise2/bambu.sh
@@ -4,7 +4,7 @@ export PATH=/opt/panda/bin:$PATH
 mkdir -p search
 cd search
 echo "#simulation of search function"
-bambu ../tree.c --top-fname=main --top-rtldesign-name=search --simulate -v2 -DNDEBUG -DBAMBU_PROFILING --simulator=ICARUS --generate-tb=../test_search.xml -v2 --print-dot 2>&1 | tee pop.log
+bambu ../tree.c --top-fname=main --top-rtldesign-name=search --simulate -v2 -DNDEBUG -DBAMBU_PROFILING --simulator=ICARUS --generate-tb=../test_search.xml -v2 --print-dot -O3 --experimental-setup=BAMBU 2>&1 | tee search.log
 cd ..
 
 
